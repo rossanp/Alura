4c7bc5228bf128ac78566b9915d6cdaf7dbb1e59
Rossan P <53863541+rossanp@users.noreply.github.com>
2022-03-19 15:43:37 -0300

    Inicio do estudo

---
+++ Trilha-FormacaoReactComTypeScript/alura-studies/src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Botao extends React.Component {
+    render() {
+        return (
+            <button style={{
+                backgroundColor: "blue"
+            }}>
+                Botão
+            </button>
+        ) 
+    }
+}
+
+export default Botao;
